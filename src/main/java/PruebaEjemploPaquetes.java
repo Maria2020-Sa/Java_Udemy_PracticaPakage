bd91b41c0ca7e31949868d38479a823997501b65
Maria2020-Sa <maugesara@gmail.com>
2024-01-08 13:58:52 -0300

    practica pakage-final

---
+++ src/main/java/PruebaEjemploPaquetes.java
@@ -0,0 +1,21 @@
+
+//import com.gm.Utileria;
+
+//import com.gm.*;
+
+import static com.gm.Utileria.imprimir;
+public class PruebaEjemploPaquetes {
+    public static void main(String[] args) {
+        //Utilizando el nombre de la clase completamete calificado.
+        //En Ingles: fully qualified name.
+        //com.gm.Utileria.imprimir("Hola");
+
+        //Haciendo un import de la clase.
+        //Utileria.imprimir("Chau!!");
+
+        //Haciendo un import satic
+        imprimir("Hola y Chau");
+
+    }
+
+}
---
+++ src/main/java/com/palabrafinal/ClaseFinal.java
@@ -0,0 +1,12 @@
+package com.palabrafinal;
+
+public final class ClaseFinal {
+    //Variable marcada como FINAL: solo se puede asignar valor en esta misma variable o dentro de un constructor,
+    // se debe asignar un valor ya que el mismo no podra ser modificado.
+    public final int varNumero = 10;
+
+
+}
+
+//No es posible crear clases Hijas de una clase marcada como FINAL.
+//class ClaseHija extends ClaseFinal {}
